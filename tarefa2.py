1cee6cc67f4e2e3ab3b2f64cf53766e857513361
theyujisamfull <theyujisamfulll@gmail.com>
2018-05-29 01:55:03 -0300

    3

--- tarefa2.py
+++ tarefa2.py
@@ -1,7 +1,7 @@
 
 matriz = []
 
-for i in range(1,21):
+for i in range(0,20):
     grupo = i
     cacique = int ((grupo*(grupo+1))/2 )
     numero_de_indios = grupo+1
@@ -16,13 +16,21 @@ for i in range(1,21):
 quem_sao_os_caciques = []
 for i in range(1,21):
     quem_sao_os_caciques.append( int( (i*(i+1))/2)  )
+'''
+
+matriz_de_ligacao=[]
+for i in range(1,231):
+    x = 20*[0]
+    for j in matriz[i]:
+        x[j-1]=1
+
+'''
+
+#denominador_cacique = len(matriz[i])
+s=0
+for i in range(0,20):
+   s = s+ len(matriz[i])
+print(len(matriz[0]) )
 
 
-matriz_de_
-
-x1 = 20*[0]
-for i in matriz[0]:
-    x1[i-1]=1
-
-print(x1)
-
+if 1 in quem_sao_os_caciques: print('oi')
