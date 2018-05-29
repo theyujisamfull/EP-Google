0f0a9d21db181a1a90316326a8e12235ad676baa
Guilherme Fernandes <aateg@live.com>
2018-05-29 01:51:54 -0300

    matriz denominadores

--- tarefa2.py
+++ tarefa2.py
@@ -11,4 +11,14 @@ for i in range(1,21):
         
     matriz.append( matriz_do_grupo )
 
-print(matriz)
+matriz_den = []
+for i in range(20):
+    for j in range(len(matriz[i])):
+        if (j==0): # significa que é cacique
+            den_cacique=len(matriz[i])-1+20-1 # 20 caciques
+            matriz_den[i].append(1/den_cacique)
+        else: # significa que é indio
+            den_indio=len(matriz[i])-1
+            matriz_den[i].append(1/den_indio)
+    
+print(matriz_den)
