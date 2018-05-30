178a30be9ef46640eef95e33d31b9be37f19eea9
rubens-bolgheroni <rubens.bolgheroni@gmail.com>
2018-05-30 01:24:13 -0300

    Vetores V,L e C
    Adiciona os vetores L e C e melhora V

--- tarefa2.py
+++ tarefa2.py
@@ -2,10 +2,10 @@
 
 
 matriz = []
-numeros_caciques = [] # quem é cacique
+numeros_caciques = [] # quem  cacique
 for i in range(1,21):
     grupo = i
-    cacique = int ((grupo*(grupo+1))/2 )
+    cacique = int((grupo*(grupo+1))/2 )
     numeros_caciques.append(cacique)
     numero_de_indios = grupo+1
     matriz_do_grupo = []
@@ -19,10 +19,10 @@ matriz_den = []
 for i in range(20):
     m_den=[]
     for j in range(len(matriz[i])):
-        if (j==0): # significa que é cacique
+        if (j==0): # significa que cacique
             den_cacique=len(matriz[i])-1+20-1 # 20 caciques
             m_den.append(1/den_cacique)
-        else: # significa que é indio
+        else: # significa que indio
             den_indio=len(matriz[i])-1
             m_den.append(1/den_indio)
     matriz_den.append(m_den)
@@ -41,12 +41,12 @@ def qual_grupo_e_pertence(e):
                    
                    
 
-#Declara matriz de ligação 
+#Declara matriz de lig 
 matriz_de_ligacao=[]
 
 for e in range(1,231):
     #Cada elemento z da linha e da matriz_de_ligacao diferente de zero quer dizer que z aponta pra e
-    #Se i é cacique sabe-se que todos os outros caciques apontam pra i
+    #Se i  cacique sabe-se que todos os outros caciques apontam pra i
     x = 230*[0]
     if e in numeros_caciques: 
         grupo_que_e_pertence = qual_grupo_e_pertence(e)
@@ -65,10 +65,27 @@ for e in range(1,231):
 
 V=[]
 for i in range(0,230):
-    for j in matriz_de_ligacao[i]:
-        if j!=0: V.append(j)
+    for j in range(0,len(matriz_de_ligacao[i])):
+        if matriz_de_ligacao[i][j]!=0: V.append(matriz_de_ligacao[i][j])
 
 
+L=[]
+for i in range(0,230):
+    for j in range(0,len(matriz_de_ligacao[i])):
+        if matriz_de_ligacao[i][j]!=0: L.append(i)
+
+
+C=[]
+for i in range(0,230):
+    for j in range(0,len(matriz_de_ligacao[i])):
+        if matriz_de_ligacao[i][j]!=0: C.append(j)
+
+
+
+
+
+
+print(C)
 
 
 
